6ff30c482c3219267957e95b1c6ebfbdfe1200ca
Akhil <kas032025d@gmail.com>
2025-08-29 10:19:46 +0530

    Comment out Bootstrap JS script in Video_gallery.php

--- app/Views/Video_gallery.php
+++ app/Views/Video_gallery.php
@@ -112,7 +112,7 @@
 </div>
 
 <!-- Bootstrap JS -->
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
+<!-- <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script> -->
 <script>
   const videoModal = document.getElementById('videoModal');
   const videoFrame = document.getElementById('videoFrame');
